6b0111d3cd71436d763422eca767812281a76d5a
Nicholas Bazos <nbazos15@gmail.com>
2020-03-29 19:28:02 -0400

    Conversation Option Select System
    Works with David's delayed dialogue system

---
+++ Assets/Scripts/ChoiceScript.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChoiceScript : MonoBehaviour
+{
+    public GameObject dialogueText;
+    public GameObject choice01;
+    public GameObject choice02;
+    public GameObject choice03;
+    int choiceMade = 0;
+    public bool choiceCompleted = false;
+
+    int choiceTextIndex1;
+    int choiceTextIndex2;
+    int choiceTextIndex3;
+
+    public void Start()
+    {
+        choice01.GetComponentsInChildren<Text>()[0].text = "Likewise! What are you interested in?";
+        choice02.GetComponentsInChildren<Text>()[0].text = "Fantastic! I am excited too!";
+        choice03.GetComponentsInChildren<Text>()[0].text = "We will have to think of something cool to do together!";
+
+        choiceTextIndex1 = 0;
+        choiceTextIndex2 = 1;
+        choiceTextIndex3 = 2;
+    }
+
+    // Button 1 invokes this
+    public void ChoiceOption1()
+    {
+        gameObject.GetComponent<DialogueManager>().currentTextIndex = choiceTextIndex1;
+        choiceMade = 1;
+        choiceCompleted = true;
+    }
+
+    // Button 2 invokes this
+    public void ChoiceOption2()
+    {
+        gameObject.GetComponent<DialogueManager>().currentTextIndex = choiceTextIndex2;
+        choiceMade = 2;
+        choiceCompleted = true;
+    }
+
+    // Button 3 invokes this
+    public void ChoiceOption3()
+    {
+        gameObject.GetComponent<DialogueManager>().currentTextIndex = choiceTextIndex3;
+        choiceMade = 3;
+        choiceCompleted = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Do things depending on choice made
+
+        if(choiceMade == 1)
+        {
+            SetChoiceText("Further option #1", "Further option #2", "Further option #3");
+            // can set choice text index here as well 
+        }
+        if (choiceMade == 2)
+        {
+            SetChoiceText("Further option #1", "Further option #2", "Further option #3");
+        }
+        if (choiceMade == 3)
+        {
+            SetChoiceText("Further option #1", "Further option #2", "Further option #3");
+        }
+    }
+
+    void ResetChoiceText()
+    {
+        choice01.GetComponentsInChildren<Text>()[0].text = "";
+        choice02.GetComponentsInChildren<Text>()[0].text = "";
+        choice03.GetComponentsInChildren<Text>()[0].text = "";
+    }
+
+    void SetChoiceText(string choice01, string choice02, string choice03)
+    {
+        this.choice01.GetComponentsInChildren<Text>()[0].text = choice01;
+        this.choice02.GetComponentsInChildren<Text>()[0].text = choice02;
+        this.choice03.GetComponentsInChildren<Text>()[0].text = choice03;
+    }
+}
--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -8,7 +8,7 @@ public class DialogueManager : MonoBehaviour
     // Start is called before the first frame update
     enum DialogueState {Off,Writing,Done }
     DialogueState currentDialogueState = DialogueState.Off;
-    int currentTextIndex = 0;
+    public int currentTextIndex = 0;
 
     int currentTextStringIndex = 0;
     float writingDelay = 0.01f;
@@ -19,7 +19,8 @@ public class DialogueManager : MonoBehaviour
     {
         "Hello! My Name is ----- and I'm looking forward to having a great time with you!",
         "I'm really into sports and social gatherings, but sadly due to the virus I can't do any of that...",
-        "So maybe we can talk and get to know each other better! As long as we remember to not get too close ;)"
+        "So maybe we can talk and get to know each other better! As long as we remember to not get too close ;)",
+        "What fun activities can we do in quarintine?"
     };
     void Start()
     {
@@ -30,7 +31,7 @@ public class DialogueManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return)) 
+        if (gameObject.GetComponent<ChoiceScript>().choiceCompleted) 
         {
             if (currentDialogueState == DialogueState.Writing)
             {
@@ -45,6 +46,9 @@ public class DialogueManager : MonoBehaviour
                 dialogue.GetComponent<Text>().text = "";
                 lastCharacterWrite = 0;
                 currentDialogueState = DialogueState.Writing;
+
+                
+                gameObject.GetComponent<ChoiceScript>().choiceCompleted = false;
             }
 
         }
