28d7c65933343ad487f2f0c5bfe8144808276911
David Knolls <Therobodavo@gmail.com>
2020-03-30 21:56:13 -0400

    fixed emotions

--- Assets/Scripts/DavidsCharacterManager.cs
+++ Assets/Scripts/DavidsCharacterManager.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DavidsCharacterManager : MonoBehaviour
 {
+    public Sprite normal;
+    public Sprite happy;
+    public Sprite cry;
+    public Sprite angry;
+    public Sprite mask;
+
+    public GameObject character;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +21,42 @@ public class DavidsCharacterManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        DialogueDistributor.PartnerEmotion status = this.gameObject.GetComponent<DialogueDistributor>().emot;
-        //happy, cry, angry, normal, mask
-        //if(status == DialogueDistributor.PartnerStatus.)
+        DialogueDistributor.PartnerEmotion status = this.gameObject.GetComponent<DialogueDistributor>().emotionByText[this.gameObject.GetComponent<DialogueDistributor>().scenarioID,this.gameObject.GetComponent<PlayManager>().currentTextIndex];
+        //happy, cry, angry, normal, mask 
+        if (status == DialogueDistributor.PartnerEmotion.Normal)
+        {
+            if (normal && character) 
+            {
+                character.GetComponent<Image>().sprite = normal;
+            }
+        }
+        else if (status == DialogueDistributor.PartnerEmotion.Mask)
+        {
+            if (mask && character)
+            {
+                character.GetComponent<Image>().sprite = mask;
+            }
+        }
+        else if (status == DialogueDistributor.PartnerEmotion.Happy)
+        {
+            if (happy && character)
+            {
+                character.GetComponent<Image>().sprite = happy;
+            }
+        }
+        else if (status == DialogueDistributor.PartnerEmotion.Cry)
+        {
+            if (cry && character)
+            {
+                character.GetComponent<Image>().sprite = cry;
+            }
+        }
+        else if (status == DialogueDistributor.PartnerEmotion.Angry) 
+        {
+            if (angry && character)
+            {
+                character.GetComponent<Image>().sprite = angry;
+            }
+        }
     }
 }
