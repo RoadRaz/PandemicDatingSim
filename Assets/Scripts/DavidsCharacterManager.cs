ab23b875cbf2cc22f69b85cf34d98e9c81b49c5c
David Knolls <Therobodavo@gmail.com>
2020-03-30 21:07:04 -0400

    added my script

---
+++ Assets/Scripts/DavidsCharacterManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DavidsCharacterManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DialogueDistributor.PartnerEmotion status = this.gameObject.GetComponent<DialogueDistributor>().emot;
+        //happy, cry, angry, normal, mask
+        //if(status == DialogueDistributor.PartnerStatus.)
+    }
+}
